93d87c8c1528bb4d4ddf053be28d44a6236ead40
cdothnh <58654742+cdothnh@users.noreply.github.com>
2021-12-08 15:05:30 +0700

    Add files via upload

---
+++ server_MLP.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+from flask.templating import render_template
+
+import back_MLP
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+   	return render_template("app.html")
+
+@app.route('/', methods = ['GET','POST'])
+def classify_image():
+    image_data = request.form['image_data']
+
+    response = jsonify(back_MLP.classify_image(image_data))
+
+    response.headers.add('Access-Control-Allow-Origin','*')
+
+    return  response
+    
+
+if __name__ == "__main__":
+    back_MLP.load_saved_model()
+    app.run(port=3000, debug=True)
